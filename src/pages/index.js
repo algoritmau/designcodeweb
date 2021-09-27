98905a3a2d6efb137e132f655999a9db93a0d5e2
Mauricio Paternina <spaceinvadev@icloud.com>
2021-09-27 17:51:08 -0500

    add HeroSection styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { Link } from 'gatsby'
-import Layout from '../components/layout/layout'
-import SEO from '../components/layout/seo'
-import HeroSection from '../components/sections/HeroSection'
+
+import Layout from '@components/layout/layout'
+import SEO from '@components/layout/seo'
+import HeroSection from '@components/sections/HeroSection'
 
 function IndexPage() {
   return (
