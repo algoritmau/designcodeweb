4c3e9116d2b521696e40b9c54926caf3369890fa
Mauricio Paternina <spaceinvadev@icloud.com>
2021-10-08 16:56:38 -0500

    add wave backgrounds and text animation

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -1,15 +1,17 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 
 import { H1, MediumText } from '@styles/TextStyles'
 import { themes } from '@styles/ColorStyles'
 
 import PurchaseButton from '@components/buttons/PurchaseButton'
 import MockupAnimation from '@components/animations/mockupAnimation'
+import WaveBackground from '@components/backgrounds/WaveBackground'
 
 export default function HeroSection() {
   return (
     <Wrapper>
+      <WaveBackground />
       <ContentWrapper>
         <TextWrapper>
           <Title>
@@ -32,26 +34,54 @@ export default function HeroSection() {
   )
 }
 
+const animation = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(-10px);
+    filter: blur(10px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+    filter: blur(0);
+  }
+`
+
 const Wrapper = styled.section`
-  background: linear-gradient(180deg, #4316db 0%, #9076e7 100%);
-  padding-left: 24px;
   overflow: hidden;
 `
 
 const ContentWrapper = styled.div`
   max-width: 1234px;
   margin: 0 auto;
-  padding: 200px 30px;
+  padding: 200px 32px 200px 56px;
 
   display: grid;
   grid-template-columns: auto 1fr;
-  gap: 40px;
+  gap: 64px;
 `
 
 const TextWrapper = styled.div`
   max-width: 360px;
   display: grid;
   gap: 30px;
+
+  & > * {
+    opacity: 0;
+    animation: ${animation} 1.6s forwards;
+
+    :nth-child(1) {
+      animation-delay: 0s;
+    }
+
+    :nth-child(2) {
+      animation-delay: 0.2s;
+    }
+
+    :nth-child(3) {
+      animation-delay: 0.4s;
+    }
+  }
 `
 
 const Title = styled(H1)`
